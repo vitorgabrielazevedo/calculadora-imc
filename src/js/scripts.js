59b9403011efbba6da17e0ad9c7c66247fa08a03
Vitor Gabriel <vs569119@gmail.com>
2024-07-07 14:42:42 -0300

    iniciando scripts

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,26 @@
-console.log("teste");
+// selecionando dados
+
+const alturaInput = document.querySelector("#altura");
+const pesoInput = document.querySelector("#peso");
+const calcBtn = document.querySelector("#calc-btn");
+const limparBtn = document.querySelector("#limpar-btn");
+
+// funções
+
+const calcularImc = (altura, peso) => {
+  const imc = peso / altura ** 2;
+
+  console.log(imc);
+};
+
+// eventos
+
+calcBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const altura = alturaInput.value;
+
+  console.log(altura);
+
+  //   calcularImc(altura, peso);
+});
