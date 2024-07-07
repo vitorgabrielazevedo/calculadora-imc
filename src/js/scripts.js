d7da9998ea7e745cf2b45fd6fa96807a6a1aef1f
Vitor Gabriel <vs569119@gmail.com>
2024-07-07 20:03:49 -0300

    correção de alguns detalhes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,44 @@
+const dados = [
+  {
+    min: 0,
+    max: 18.4,
+    classificacao: "Menor que 18,5",
+    info: "Baixo peso",
+    obesidade: "0",
+  },
+  {
+    min: 18.5,
+    max: 24.9,
+    classificacao: "Entre 18,5 e 24,9",
+    info: "Peso normal",
+    obesidade: "0",
+  },
+  {
+    min: 25,
+    max: 29.9,
+    classificacao: "Entre 25 e 29,9",
+    info: "Sobrepeso",
+    obesidade: "I",
+  },
+  {
+    min: 30,
+    max: 39.9,
+    classificacao: "Entre 30 e 39,9",
+    info: "Obesidade",
+    obesidade: "II",
+  },
+  {
+    min: 40,
+    max: 99,
+    classificacao: "Maior que 40",
+    info: "Obesidade grave",
+    obesidade: "III",
+  },
+];
+
 // selecionando dados
 
+const imcTable = document.querySelector("#imc-table");
 const alturaInput = document.querySelector("#altura");
 const pesoInput = document.querySelector("#peso");
 const calcBtn = document.querySelector("#calc-btn");
@@ -7,10 +46,26 @@ const limparBtn = document.querySelector("#limpar-btn");
 
 // funções
 
-const calcularImc = (altura, peso) => {
-  const imc = peso / altura ** 2;
+const criarTable = (dados) => {
+  dados.forEach((item) => {
+    const div = document.createElement("div");
+    div.classList.add("table-dado");
 
-  console.log(imc);
+    const classificacao = document.createElement("p");
+    classificacao.innerText = item.classificacao;
+
+    const info = document.createElement("p");
+    info.innerText = item.info;
+
+    const obesidade = document.createElement("p");
+    obesidade.innerText = item.obesidade;
+
+    div.appendChild(classificacao);
+    div.appendChild(info);
+    div.appendChild(obesidade);
+
+    imcTable.appendChild(div);
+  });
 };
 
 // eventos
@@ -18,9 +73,5 @@ const calcularImc = (altura, peso) => {
 calcBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  const altura = alturaInput.value;
-
-  console.log(altura);
-
-  //   calcularImc(altura, peso);
+  console.log("hahaha");
 });
